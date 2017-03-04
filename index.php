96b06bdbf907f3d8b6a361dca0fe431423b5e416
JanuriDP <janury.geek@gmail.com>
2017-03-05 00:34:13 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?include("config.php");include("login.php");?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <script src="//code.jquery.com/jquery-latest.js"></script>
+  <script src="chat.js"></script>
+  <link href="chat.css" rel="stylesheet"/>
+  <title>PHP Group Chat With jQuery & AJAX</title>
+ </head>
+ <body>
+  <div id="content" style="margin-top:10px;height:100%;">
+   <center><h1>Group Chat In PHP</h1></center>
+   <div class="chat">
+    <div class="users">
+     <?include("users.php");?>
+    </div>
+    <div class="chatbox">
+     <?
+     if(isset($_SESSION['user'])){
+      include("chatbox.php");
+     }else{
+      $display_case=true;
+      include("login.php");
+     }
+     ?>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
